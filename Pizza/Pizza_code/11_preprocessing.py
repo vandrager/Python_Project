8a4b066bc81a2b1ccc919276b7c948e84a9f8470
vandrager <vandrager@naver.com>
2021-01-25 17:49:56 +0900

    preprocessing_finish

--- Pizza/Pizza_code/11_preprocessing.py
+++ Pizza/Pizza_code/11_preprocessing.py
@@ -6,11 +6,10 @@ import datetime
 
 os.chdir(r"C:\Users\vandr\OneDrive\바탕 화면\Bigdata\Project_python\Pizza\Dataset")
 
-df = pd.read_excel("seoul_19.xlsx")
-print(len(df))
-# df.reset_index()
-# df.drop_duplicates(['브랜드', '일자', '주문메뉴', '리뷰'])
-# df.drop(['Unnamed: 0', "Unnamed: 0.1"], axis=1, inplace=True)
+df = pd.read_excel("seoul_total.xlsx")
+df.reset_index()
+df.drop_duplicates(['브랜드', '일자', '주문메뉴', '리뷰'])
+df.drop(['Unnamed: 0', "Unnamed: 0.1"], axis=1, inplace=True)
 
 # 지역 전처리
 location = df['자치구'].str.split(' ') # 문자열을 split 메소드로 분리
@@ -39,11 +38,30 @@ time = df['일자'].str.split(" ")
 df['연'] = time.str.get(0)
 df['월'] = time.str.get(1)
 df['일'] = time.str.get(2)
-df['date'] = df['연'] + df['월'] + df['일']
 
-# 1. 요일 컬럼 만들고 2020-12-25 날짜 형식으로 만들어주자
-df.drop("일자", axis=1, inplace=True)
+# 문자열을 슬라이싱해서 자르기 (년, 월, 일 제거)
+df["연"] = df['연'].str.slice(start=0, stop=-1)
+df["월"] = df['월'].str.slice(start=0, stop=-1)
+df["일"] = df['일'].str.slice(start=0, stop=-1)
+df['date'] = df['연'] + "-" + df['월'] + "-" + df['일']
+df['date'] = pd.to_datetime(df['date'])
+df['weekday'] = df['date'].dt.strftime("%A")
+
+# 주문메뉴 전처리
+menu = df['주문메뉴'].str.split(",")
+df['메인메뉴'] = menu.str.get(0)
+df['메뉴2'] = menu.str.get(1)
+df['메뉴3'] = menu.str.get(2)
+df['메뉴4'] = menu.str.get(3)
+df['단품주문'] = df['메뉴2'].isnull()
+df['다량주문'] = df['메뉴4'].notnull()
+
 
 # 열 순서 바꿔주고 저장하기
-df.columns = ["딘딘 네모의꿈"]
-df.to_excel("erra.xlsx")
+df.drop(["일자", "메뉴2", "메뉴3", "메뉴4"], axis=1, inplace=True)
+df = df[['시', '구', '브랜드명', '지점명', '주문메뉴', '메인메뉴', '별점', '맛', '양', '배달', '리뷰', '연', '월', '일', 'date', 'weekday', '단품주문', '다량주문']]
+
+df.to_excel("seoul_review.xlsx")
+
+print(df.head())
+print(df.info())
