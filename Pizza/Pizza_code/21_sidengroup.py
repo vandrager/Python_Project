0382d4a3243e58af2080aaf3abe5a254da4e9476
vandrager <vandrager@naver.com>
2021-02-16 23:39:50 +0900

    finish_processing

--- Pizza/Pizza_code/21_sidengroup.py
+++ Pizza/Pizza_code/21_sidengroup.py
@@ -5,7 +5,7 @@ import numpy as np
 
 os.chdir(r"C:\Users\vandr\OneDrive\바탕 화면\Bigdata\Project_python\Pizza\Dataset\1차 분석")
 
-df = pd.read_excel("Review_5.xlsx")
+df = pd.read_excel("Review_6.xlsx")
 
 '''
 # 사이드 메뉴 전처리
@@ -25,12 +25,22 @@ df['메뉴구분'].replace("치즈오븐스파게티", "기타", inplace = True)
 세트2（피자L＋스파게티＋핫윙＋콜라1.25L）
 세트5（피자R＋스파게티＋콜라500ml）
 세트6（피자R＋스파게티＋핫윙＋콜라1.25L)
------피자헛------
 
 
 df.to_excel("Review_4.xlsx")
 '''
 
 
-gdf = df.groupby(['브랜드명', '메뉴구분'])['별점'].count()
+# 국내산오이피클
+# 사이다1.25L
+# 콜라（펩시）1.25L
+# 기타
+
+df[df['메뉴구분'] == "사이다1.25L"] = "기타"
+df[df['메뉴구분'] == "콜라（펩시）1.25L"] = "기타"
+df[df['메뉴구분'] == "국내산오이피클"] = "기타"
+ndf = df[df['메뉴구분'] != "기타"]
+ndf.to_excel("Review_7.xlsx")
+
+gdf = ndf.groupby(['브랜드명', '메뉴구분'])['별점'].count()
 gdf.to_excel("ff.xlsx")
