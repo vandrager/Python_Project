52e5690ee02c944e89ac748bdaf7b66b90b43017
vandrager <vandrager@naver.com>
2021-02-26 15:39:14 +0900

    listup_3

--- job/5_digital_marketing.py
+++ job/5_digital_marketing.py
@@ -23,34 +23,61 @@ write_ws = write_wb.active
 write_ws.append(["회사명", "공고명", "기간", "링크"])
 
 # 공고 선택 조건 세팅(마케팅/신입/지역(서울, 경기))
-elem = browser.find_element_by_xpath("//*[@id='devSearchForm']/div[2]/div/div[1]/dl[1]/dd[2]/div[2]/dl[1]/dd/div[1]/ul/li[2]/label")
+elem = browser.find_element_by_xpath("//*[@id='devSearchForm']/div[2]/div/div[1]/dl[1]/dd[2]/div[2]/dl[1]/dd/div[1]/ul/li[2]/label") # 마케팅
 elem.click()
 
 elem = browser.find_element_by_xpath("//*[@id='duty_step2_10013_ly']/li[1]/label")
 elem.click()
 
-elem = browser.find_element_by_xpath("//*[@id='devSearchForm']/div[2]/div/div[1]/dl[3]/dt/p")
+elem = browser.find_element_by_xpath("//*[@id='devSearchForm']/div[2]/div/div[1]/dl[3]/dt/p") # 신입
 elem.click()
 
 elem = browser.find_element_by_xpath("//*[@id='devSearchForm']/div[2]/div/div[1]/dl[3]/dd/div[1]/ul[1]/li[1]/label")
+elem.click()
+
+elem = browser.find_element_by_xpath("//*[@id='devSearchForm']/div[2]/div/div[1]/dl[2]/dt/p") # 지역(서울/경기)
+elem.click()
+
+elem = browser.find_element_by_xpath("//*[@id='devSearchForm']/div[2]/div/div[1]/dl[2]/dd[2]/div[2]/dl[1]/dd/div[1]/ul/li[1]/label")
+elem.click()
+
+elem = browser.find_element_by_xpath("//*[@id='devSearchForm']/div[2]/div/div[1]/dl[2]/dd[2]/div[2]/dl[1]/dd/div[1]/ul/li[2]/label")
 elem.click()
 
 elem = browser.find_element_by_xpath("//*[@id='dev-btn-search']")
 elem.click()
 
 # 헤드라인, 추천, 핵심 채용관 정보 추가
+time.sleep(interval) # 주요채용
+soup = bs(browser.page_source, "lxml")
+h_here = soup.find_all("li", attrs={"class": "itemBgTop devloopArea"})
+for h in h_here:
+    try:
+        c = h.find("div", attrs={"class": "company"})
+        t = h.find("div", attrs={"class": "description"})
+        d = h.find("span", attrs={"class": "deadLine"})
+        l = h.find("a", attrs={"class": "effectLog"})
+        company = c.text.strip()
+        title = t.text.strip()
+        date = d.text.strip()
+        link = "http://www.jobkorea.co.kr/"+l['href']
+    except:
+        pass
+    write_ws.append([company, title, date, link])
+
 time.sleep(interval)
 soup = bs(browser.page_source, "lxml")
 h_here = soup.find_all("li", attrs={"class": "itemBg devloopArea"})
 for h in h_here:
     try:
         c = h.find("div", attrs={"class": "company"})
-        t = h.find("a", attrs={"class": "effectLog"})
+        t = h.find("div", attrs={"class": "description"})
         d = h.find("span", attrs={"class": "deadLine"})
+        l = h.find("a", attrs={"class": "effectLog"})
         company = c.text.strip()
         title = t.text.strip()
         date = d.text.strip()
-        link = "http://www.jobkorea.co.kr/"+t['href']
+        link = "http://www.jobkorea.co.kr/"+l['href']
     except:
         pass
     write_ws.append([company, title, date, link])
@@ -60,12 +87,13 @@ h_here = soup.find_all("li", attrs={"class": " devloopArea"})
 for h in h_here:
     try:
         c = h.find("div", attrs={"class": "company"})
-        t = h.find("a", attrs={"class": "effectLog"})
+        t = h.find("div", attrs={"class": "description"})
         d = h.find("span", attrs={"class": "deadLine"})
+        l = h.find("a", attrs={"class": "effectLog"})
         company = c.text.strip()
         title = t.text.strip()
         date = d.text.strip()
-        link = "http://www.jobkorea.co.kr/"+c['href']
+        link = "http://www.jobkorea.co.kr/"+l['href']
     except:
         pass
     write_ws.append([company, title, date, link])
@@ -88,7 +116,7 @@ for i in range(1, 11):
             pass
         write_ws.append([company, title, date, link])
     
-    write_wb.save("maketing{}.xlsx".format(today))
+    write_wb.save("maketing_{}.xlsx".format(today))
     if i == 1:
         break
 
